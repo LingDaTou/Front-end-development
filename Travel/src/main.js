e97420736db90218f104fc045a5604444d06225f
ling <153861690@qq.com>
2019-05-16 20:44:45 +0800

    add

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -2,14 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import router from './router'
 import fastClick from 'fastClick'
+
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 
+
 Vue.config.productionTip = false
 fastClick.attach(document.body)   //移动端点击延迟300毫秒
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({

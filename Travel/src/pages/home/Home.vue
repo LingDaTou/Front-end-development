c97a5b0409de51692c11298f95958d9cef27f285
ling <153861690@qq.com>
2019-05-16 15:15:13 +0800

    Travel

---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		hello world
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		name:"Home"
+	}
+
+</script>
+
+<style type="text/css" scoped>
+
+</style>

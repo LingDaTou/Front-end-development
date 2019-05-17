0dd7aee4f315150db0cda912573501a68049733b
ling <153861690@qq.com>
2019-05-17 21:24:05 +0800

    add city

---
+++ Travel/src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="/">
+            <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:"CityHeader"    
+}
+</script>
+
+
+<style lang="stylus" scoped>
+
+.header
+  position:relative
+  line-height:2rem
+  background:#00bcd4
+  color:#fff
+  text-align:center
+
+.header-back
+    position:absolute
+    top:0
+    left :0
+    width :.64rem
+    text-align :center
+    font-size:20px
+    color:#fff
+</style>
--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -6,9 +6,11 @@
 		<div class="header-input">
 			<span class="iconfont">&#xe632;</span>
 			输入城市/景点/游玩主题</div>
-		<div class="header-right">{{this.city}}
-			<span class="iconfont arrow-icon">&#xe64a;</span>
-		</div>
+		<router-link to="/city">
+			<div class="header-right">{{this.city}}
+				<span class="iconfont arrow-icon">&#xe64a;</span>
+			</div>
+		</router-link>
 	</div>
 </template>
 
@@ -56,6 +58,7 @@
 	width:3.5rem
 	float:right
 	text-align:center
+	color :white
 
 .arrow-icon
 	font-size :.24rem

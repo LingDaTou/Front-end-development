036b31a7ba10dc559d1ad7886c02fafd64d5905b
ling <153861690@qq.com>
2019-05-21 14:33:43 +0800

    add city-search

---
+++ Travel/src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search">
+       <input type="text" placeholder="输入城市名或拼音" class="search-input">
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:"CitySearch"    
+}
+</script>
+
+
+<style lang="stylus" scoped>
+.search
+      background:#00bcd4
+      height :2rem
+      padding:1px 3px
+      
+      .search-input
+          box-sizing:border-box
+          padding:1px 1px
+          line-height :1.7rem
+          width :100%
+          text-align :center
+          border-radius:5px
+          color :#666
+</style>

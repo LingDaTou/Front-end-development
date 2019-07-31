72bb20c9d31a5a1d5137868fb2310309e0ecda8f
ling <153861690@qq.com>
2019-07-31 13:03:02 +0800

    update

--- Projects1/game2_Snack/js/SnackMap.js
+++
@@ -1,8 +0,0 @@
-class SnackMap {
-    constructor(){
-        let oDiv=document.createElement("div");
-        oDiv.className="map";
-        document.body.appendChild(oDiv);
-        this.oMap=oDiv;
-    }
-}
--- Projects2/JSONP_ajax/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$func_name =  $_GET["t"];
-//echo $func_name."(20)";
-
-echo "hi";
-//$cb = $_GET["cb"];
-//echo $cb."('')";
